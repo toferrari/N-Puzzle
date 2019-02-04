f955f335b73de0d5c279b89c0bfc2670918b5d66
Fanny HUANG <fhuang@e1r2p20.42.fr>
2019-02-04 18:44:29 +0100

    [FIX] functions default arguments

--- heuristics.py
+++ heuristics.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-def manhattan_distance(A={x: 0, y:0}, B={x:0, y:0}):
+def manhattan_distance(A, B):
 	return abs(B['x'] - A['x']) + abs(B['y'] - A['y'])
 
 
-def euclid_distance(A={x: 0, y:0}, B={x:0, y:0}):
+def euclid_distance(A, B):
 	return ((B['x'] - A['x'])**2 + (B['y'] - A['y'])**2)**1/2
