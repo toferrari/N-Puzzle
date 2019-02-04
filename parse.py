cb4abc47eea57e2bb5ad89605018ef8484277dc0
Tony FERRARI <tferrari@e1r3p19.42.fr>
2019-02-04 19:23:35 +0100

    parsing done

--- parse.py
+++ parse.py
@@ -12,33 +12,70 @@ def helper():
 def lamdba_remove_list_empty(line):
 	return(list(filter(lambda word: word != "", line)))
 
+def get_index(line):
+	for x, word in enumerate(line):
+		for letter in word:
+			if letter == '#':
+				return (x)
+	return (0)
+
+def remove_htag(line):
+	try:
+		index = get_index(line)
+		if index > 0:
+			line = line[:index]
+	except:
+		pass
+	return(line)
+
+def convert_int(word):
+	try:
+		number = int(word)
+		return (number)
+	except:
+		return (-1)
+
+def check_duplicate(puzzle, lines):
+	for y, row in enumerate(puzzle):
+		for x, number in enumerate(row):
+			if (number < 0):
+				error ("wrong number in puzzle")
+			for i in range(y, len(puzzle)):
+				for j in range(x, len(puzzle)):
+					if (i == y and j == x and number == puzzle[i][j]):
+						pass
+					elif (number == puzzle[i][j]):
+						error("error duplicate numbers")
+
+def error(message):
+	print(message)
+	exit()
+
 def get_puzzle(lines):
 	lines = list(map(lambda line : re.sub("\n", "", line), lines))
 	lines = list(map(lambda line : line.split(' '), lines))
-	lines = list(map(lambda line: lamdba_remove_list_empty(line), lines))
-	size = "a"
+	lines = list(map(lambda line : lamdba_remove_list_empty(line), lines))
+	lines = list(map(lambda line : remove_htag(line), lines))
+	size = 0
+	line_puzzle = 0
 	for line in lines:
-		print(line)
-		# print(' '.join(line))
 		if (line[0] == "#" or line[0] == '#\n'):
 			pass
-		elif (len(line) == 1):
-			try:
-				if not isinstance(size, str):
-					exit()
-				size = int(line[0])
-				puzzle = np.array([0] * size ** 2).reshape(size,size)
-			except:
-				print("Puzzle error")
-				exit()
+		elif (len(line) == 1 and size == 0):
+			size = convert_int(line[0])
+			if size < 3 or np.isnan(size):
+				error("Size invalid")
+			puzzle = np.array([0] * size ** 2).reshape(size,size)
 		else:
-			try:
-				if (len(line) != size):
-					exit()
-			except:
-				print("Puzzle error")
-				exit()
-	print (puzzle)
+			if (len(line) != size or line_puzzle >= size):
+				error ("Size is different than numbers of rows")
+			for index, word, in enumerate(line):
+				puzzle[line_puzzle][index] = convert_int(word)
+			line_puzzle += 1
+	if (size != line_puzzle):
+		error ("Size is different than numbers of rows")
+	check_duplicate(puzzle, lines)
+
 
 def parse(arg_file):
 	try:
@@ -47,7 +84,6 @@ def parse(arg_file):
 	except FileNotFoundError:
 		exit()
 	file.close()
-
 	puzzle = get_puzzle(lines)
 
 if __name__ == "__main__" :
