4be4b9aafbc6cced6ff9573be985ddf5030714c6
Tony FERRARI <tferrari@e1r4p4.42.fr>
2019-02-06 16:12:51 +0100

    add node

--- node.py
+++ node.py
@@ -2,4 +2,9 @@
 
 class Node():
 
-	def __init__(self, number x, y, parent, f_eval):
+	def __init__(self, number, x, y, parent, f_eval):
+		self.number = number
+		self.x = x
+		self.y = y
+		self.parent = parent
+		self.f_eval = f_eval
--- solve.py
+++ solve.py
@@ -1,22 +1,33 @@
+#!/usr/bin/env python3
 # coding: utf-8
-
 
 from Board import Board
 from EnumMove import Move
 import heuristics
+from Node import Node
+
+def init_node(direction, board, solved_board):
+	if board.can_shift(direction):
+		coord = board.get_coordinates_if_moved(direction)
+		x, y = coord['x'], coord['y']
+		node = Node(
+			board[y][x],
+			x, y,
+			None,
+			heuristics.manhattan_distance(
+				board.blank,
+				solved_board.find(board[y][x])
+			)
+		)
+		print()
+		print (node.number, node.x, node.y)
+		return (node)
 
 def solve(board):
 	solved_board = Board.get_ordered(board.array)
 	print(solved_board)
 	print("--------------------")
 	print(board)
-	x, y, _ = board.blank
-	for direction in Move:
-		if board.can_shift(direction):
-			x, y = board.get_coordinates_if_moved(direction).values()
-			value = board[y][x]
-			distance = heuristics.manhattan_distance(
-				board.blank,
-				solved_board.find(board[y][x])
-			)
-			print("If blank shift({}) then, distance({}) = {}".format( direction, value, distance))
+	x, y = board.blank['x'], board.blank['y']
+	nodes = list(map(lambda direction: init_node(direction, board, solved_board), Move))
+	nodes = [node for node in nodes if node]
