857c0e6f373f46797b38a44a8415f1766f9594b8
Fanny HUANG <fhuang@e1r4p3.42.fr>
2019-02-06 16:15:16 +0100

    algo in progress

--- solve.py
+++ solve.py
@@ -5,18 +5,42 @@ from Board import Board
 from EnumMove import Move
 import heuristics
 
+
+def get_min_eval_node(open_list):
+	if (len(open_list) == 1):
+		return open_list[0]
+	else:
+		return min(open_list, key=lambda node: node.f_eval)
+
+
+def node_in_list(closed_list, target):
+	for node in closed_list:
+		if  node.number == target.number and \
+			node.f_value < target.f_value:
+			return True
+	return False
+
+
 def solve(board):
 	solved_board = Board.get_ordered(board.array)
 	print(solved_board)
 	print("--------------------")
 	print(board)
-	x, y, _ = board.blank
-	for direction in Move:
-		if board.can_shift(direction):
-			x, y = board.get_coordinates_if_moved(direction).values()
-			value = board[y][x]
-			distance = heuristics.manhattan_distance(
-				board.blank,
-				solved_board.find(board[y][x])
-			)
-			print("If blank shift({}) then, distance({}) = {}".format( direction, value, distance))
+
+	#List of nodes
+	success = False
+	closed_list = []
+	open_list = [blank.board]
+	while success == False and (len(open_list) > 0):
+		min_node = get_min_eval_node(open_list)
+		if board == solved_board:
+			success = True
+		else:
+			closed_list.append(min_node)
+			neighbour_nodes = expand(min_node)
+			for node in neighbour_nodes:
+				node.parent = min_node
+				node_exists = node_in_list(closed_list, node)
+				if node_exists == False:
+					open_list.append(node)
+		closed_list.append(min_node)
