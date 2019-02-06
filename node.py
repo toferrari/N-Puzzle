cb184c6711d7b0684ce74f931b1e819f95dfa892
Fanny HUANG <fhuang@e1r4p3.42.fr>
2019-02-06 21:08:18 +0100

    Add other attributes to Node Class +  add __str__ and __repr__ methods

--- node.py
+++ node.py
@@ -2,9 +2,21 @@
 
 class Node():
 
-	def __init__(self, number, x, y, parent, f_eval):
+	def __init__(self, number, x, y, g_x=0, h_x=0, parent=None, direction=None):
 		self.number = number
 		self.x = x
 		self.y = y
 		self.parent = parent
-		self.f_eval = f_eval
+		self.direction = direction
+		self.g_x = g_x
+		self.h_x = h_x
+		self.f_x = self.g_x + self.h_x
+
+
+	def __repr__(self):
+		return "Node({}): ({}, {})".format(self.number, self.x, self.y)
+
+
+	def __str__(self):
+		return " -------------------\n| Number: {}\n| x, y: ({},{}) \n| f_eval: {}\n| Direction: {}\n -------------------\
+		".format(self.number, self.x, self.y, self.f_x, self.direction)
