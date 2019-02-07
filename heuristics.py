ee35b88d85fa331e132f302efe948f39501f50f8
Fanny HUANG <fhuang@e1r4p3.42.fr>
2019-02-07 16:30:44 +0100

    update heuristics functions

--- heuristics.py
+++ heuristics.py
@@ -3,16 +3,30 @@
 # def manhattan_distance(A, B):
 # 	return abs(B['x'] - A['x']) + abs(B['y'] - A['y'])
 
-def manhattan_distance(board, solved_board):
-	# return abs(B['x'] - A['x']) + abs(B['y'] - A['y'])
+def manhattan(state, final_state):
+	def man(Ax, Bx, Ay, By):
+		return abs(Bx - Ax) + abs(By - Ay)
+
 	distances = []
-	for y, row in enumerate(board):
+	for y, row in enumerate(state):
 		for x, cell in enumerate(row):
-			other_cell = solved_board.find(cell)
-			distances.append(
-				abs(x - other_cell['x']) + abs(y - other_cell['y'])
-			)
+			if cell == 0:
+				continue
+			other_cell = final_state.find(cell)
+			if cell != other_cell:
+				distances.append(man(other_cell['x'], x, other_cell['y'], y))
 	return sum(distances)
+
+
+def hamming(state, final_state):
+	n = 0
+	for y, row in enumerate(state):
+		for x, cell in enumerate(row):
+			if cell != 0 and state[y][x] != final_state[y][x]:
+				n += 1
+	return n
+
+
 
 def euclid_distance(A, B):
 	return ((B['x'] - A['x'])**2 + (B['y'] - A['y'])**2)**1/2
