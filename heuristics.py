24872c6023d17dc606a9f301c1a58c972440c967
Fanny HUANG <fhuang@e2r5p19.42.fr>
2019-02-09 18:22:22 +0100

    Dont create a func everytime a heuristic func is called

--- heuristics.py
+++ heuristics.py
@@ -4,14 +4,11 @@ from math import sqrt
 
 
 def manhattan(state, final_state):
-	def man(Ax, Bx, Ay, By):
-		return abs(Bx - Ax) + abs(By - Ay)
-
 	ret = 0
 	for (Ax, Ay), Acell in state.items():
 		for (Bx, By), Bcell in final_state.items():
 			if Acell != 0 and Acell == Bcell:
-				ret += man(Ax, Bx, Ay, By)
+				ret += abs(Bx - Ax) + abs(By - Ay)
 	return ret
 
 
@@ -24,14 +21,11 @@ def hamming(state, final_state):
 
 
 def euclid(state, final_state):
-	def euc(Ax, Bx, Ay, By):
-		return sqrt((Bx - Ax)**2 + (By - Ay)**2)
-
 	ret = 0
 	for (Ax, Ay), Acell in state.items():
 		for (Bx, By), Bcell in final_state.items():
 			if Acell != 0 and Acell == Bcell:
-				ret += euc(Ax, Bx, Ay, By)
+				ret += sqrt((Bx - Ax)**2 + (By - Ay)**2)
 	return ret
 
 
