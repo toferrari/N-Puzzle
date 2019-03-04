32b8b3f3f2673635d0e989b1bcbd12907c6d19e2
Tony FERRARI <tferrari@e3r3p16.42.fr>
2019-03-04 11:43:04 +0100

    add parser for uniform and greedy

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import subprocess
 import argparse
 import re
 
-import heuristics
 import utils
+import heuristics
+from Game import Game
 from error import error
 from State import State
-from Game import Game
+from EnumCost import Cost
 from parse import get_puzzle
 
 from tkinter import *
@@ -36,9 +37,13 @@ if __name__ == "__main__" :
 	parser.add_argument("-m", "--max_size", type=int, default=200, help="Max size of OPEN list. Lower than 8 means no limit.\nThe higher the number, the higher the complexity will be.")
 	parser.add_argument("-v", "--view", action="store_true", default=False, help="graphic visualization")
 	parser.add_argument("-i", "--image", type=str, default="pc.jpg", help="Path for an image.")
+	parser.add_argument("-g", "--greedy_search", type=bool, default=False, help="f(x) = h(x) | g(x) = 0")
+	parser.add_argument("-u", "--uniform_cost", type=bool, default=False, help="f(x) = g(x) | h(x) = 0")
 	parser.add_argument('file', nargs='?', type=argparse.FileType('r'))
 	args = parser.parse_args()
 
+	if (args.greedy_search and args.uniform_cost):
+		error("You can't activate -g and -u at the same time.")
 	if args.file:
 		lines = [re.sub('\n', '', line) for line in parse(args.file)]
 	else:
@@ -49,7 +54,12 @@ if __name__ == "__main__" :
 		exit()
 	args.heuristic = heuristics.choices[args.heuristic]
 	puzzle = get_puzzle(lines)
-	game = Game(utils._convert_puzzle_to_dict(puzzle), len(puzzle), heuristic=args.heuristic, max_size=args.max_size)
+	cost = Cost.BREADTH_FIRST
+	if (args.greedy_search):
+		cost = Cost.GREEDY
+	elif (args.uniform_cost):
+		cost = Cost.UNIFORM
+	game = Game(utils._convert_puzzle_to_dict(puzzle), len(puzzle), heuristic=args.heuristic, max_size=args.max_size, cost=cost)
 	print("Game is solvable ? {}".format("Yes" if game.is_solvable else "no"))
 	print(game)
 	if game.is_solvable:
